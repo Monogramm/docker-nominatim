ca6cf5e48f3a85de2e8705cb958ccebeb5396e22
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-09-30 03:20:05 +0200

    ✅ Check DB status
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/3.1/debian/test/docker_test.sh
+++ images/3.1/debian/test/docker_test.sh
@@ -14,13 +14,13 @@ if ! ping -c 10 -q nominatim ; then
     exit 2
 fi
 
-#if ! ping -c 10 -q nominatim-db ; then
-#    echo 'Nominatim Database container is not responding!'
-#    # TODO Display logs to help bug fixing
-#    #echo 'Check the following logs for details:'
-#    #tail -n 100 logs/*.log
-#    exit 4
-#fi
+if ! ping -c 10 -q nominatim-db ; then
+    echo 'Nominatim Database container is not responding!'
+    # TODO Display logs to help bug fixing
+    #echo 'Check the following logs for details:'
+    #tail -n 100 logs/*.log
+    exit 4
+fi
 
 # XXX Add your own tests
 # https://docs.docker.com/docker-hub/builds/automated-testing/
